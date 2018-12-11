ec679652f58546124ed24dd3479b0d7407c6bd86
Angie <arco527@gmail.com>
2018-12-11 15:15:31 -0500

    remove comerApi file

--- comerApi/app/controllers/products_controller.rb
+++
@@ -1,8 +0,0 @@
-class ProductsController < ApplicationController
-
-  def index
-    @products = ProductService.all
-    render json:@products
-  end
-
-end
--- comerApi/app/services/service_base.rb
+++
@@ -1,3 +0,0 @@
-class ServiceBase
-  include HTTParty
-end
